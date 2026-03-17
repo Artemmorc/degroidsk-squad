6b03c2fdec46e30ce9309d9302ffcc95a266c307
Artemmorc <artanimal2011@gmail.com>
2026-03-17 18:24:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ТВОИ КЛЮЧИ - НЕ ВЫКЛАДЫВАЙ В GITHUB!
 window.SUPABASE_URL = 'https://твой-проект.supabase.co'
 window.SUPABASE_ANON_KEY = 'твой-anon-ключ'
-window.ADMIN_PASSWORD = 'admin123' // поменяй на свой пароль
+window.ADMIN_PASSWORD = '0HMLKLOSLPARTEM' // поменяй на свой пароль
