e64072ca9042d233f748ba7abaf5b66f16de6ace
Artemmorc <artanimal2011@gmail.com>
2026-03-17 18:36:36 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// ТВОИ КЛЮЧИ - НЕ ВЫКЛАДЫВАЙ В GITHUB!
+window.SUPABASE_URL = 'https://qmjxuingxftxyxsceakr.supabase.co'
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtanh1aW5neGZ0eHl4c2NlYWtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3NjM5ODMsImV4cCI6MjA4OTMzOTk4M30.CQmUiB3jsDDrT-pj-NjirTw0r3O3jVToOeRdOfmOalU'
+window.ADMIN_PASSWORD = '0HMLKLOSLPARTEM' // поменяй на свой пароль
